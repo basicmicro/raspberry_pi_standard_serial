e450a9e9d47acc4123d4811d9884e3f51ab61824
Basicmicro <info@basicmicro.com>
2018-12-19 16:33:17 -0800

    Updated standard_serial.py

---
+++ standard_serial.py
@@ -0,0 +1,28 @@
+from serial import Serial
+from time import sleep
+
+if __name__ == "__main__":
+
+    serial_port = "/dev/ttyS0"
+    baudrate = 38400
+
+    roboclaw = Serial(serial_port, baudrate, timeout=1)
+    
+    while True:
+    
+        roboclaw.write(chr(94))
+        sleep(2)
+        roboclaw.write(chr(64))
+        sleep(2)
+        roboclaw.write(chr(32))
+        sleep(2)
+        roboclaw.write(chr(64))
+        
+        roboclaw.write(chr(223))
+        sleep(2)
+        roboclaw.write(chr(192))
+        sleep(2)
+        roboclaw.write(chr(160))
+        sleep(2)
+        roboclaw.write(chr(192))
+        sleep(2)
